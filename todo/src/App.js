536e4388d183c7c980e4eeb4916b8ecf6bd59c68
McNultyPT <mcnultypt@gmail.com>
2019-02-04 15:45:17 -0800

    adding state to App component

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Todo from './components/Todo';
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    todos: [
+      { value: 'Walk the dog.' },
+      { completed: false }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
